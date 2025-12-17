f949f5efafa7e4a2cb815267d388b0ec3c7266ac
Kyanlulw <datlc81@gmail.com>
2025-12-17 21:07:13 +0700

    nothingbig

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ class Manager():
             
             # Get the standard PyTorch dataloader
             train_loader = get_dataloader(self.dataset_name, self.src_sp, self.trg_sp, split = 'train[:500000]')
-            valid_loader = get_dataloader(self.dataset_name, self.src_sp, self.trg_sp, split = 'validation')
+            valid_loader = get_dataloader(self.dataset_name, self.src_sp, self.trg_sp, split = 'validation[:5000]')
 
             num_update_steps_per_epoch = len(train_loader)
             max_train_steps = int(constants.num_epochs) * num_update_steps_per_epoch
